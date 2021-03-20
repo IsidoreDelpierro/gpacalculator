c90c6d91311abac472dbc590848c555939681ebe
isidoredelpierro <fontadore@gmail.com>
2021-03-20 14:57:49 +0100

    Initial app rerouting

--- gpa/views.py
+++ gpa/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+	return HttpResponse("Wow! This is an <strong>amazing</strong> app.<br>Now, I can calculate my GPA")
